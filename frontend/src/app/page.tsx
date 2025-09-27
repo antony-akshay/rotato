731728ee4dec9caa5b7fd3f55501c1d8dbc2529f
DelwinPrakash <delwinprakash499@gmail.com>
2025-09-27 16:54:40 +0530

    feat: add Header component and integrate it into the RootLayout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 export default function Home() {
   return (
     <main>
-      <ConnectButton/>
-      <p className="bg-red-500">hellow owr</p>
+      
     </main>
   );
 }
